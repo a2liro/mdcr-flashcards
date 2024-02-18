1b913fbb50724ec539667fb008f76e9ec6ac7daa
Andre Liro <andreliro1945@gmail.com>
2024-02-17 21:50:23 -0300

    Correções de erros ao acessar  como objeto

--- Src/Repositories/Organization/StoreOrganizationRepository.php
+++ Src/Repositories/Organization/StoreOrganizationRepository.php
@@ -13,12 +13,10 @@ class StoreOrganizationRepository
         $data['created_at'] = date('Y-m-d H:i:s');
         $data['updated_at'] = date('Y-m-d H:i:s');
         $user = new User();
-        $user->getCurrentUser();
+        $user = $user->getCurrentUser();
         $data['user_id'] = $user['id'];
         $organization = new Organization();
         $organization->create($data);
-        $user->ownOrganizatiionList[] = $organization;
-        $user->store();
         return $organization;
     }
 }
